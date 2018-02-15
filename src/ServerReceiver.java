26324a01ff1808606153eaa41a48a7485d3de449
dxr714 <dxr714@student.bham.ac.uk>
2018-02-15 00:29:59 +0000

    started working on previous and made all cases that take a second argument check if it's null in case the client closed

--- src/ServerReceiver.java
+++ src/ServerReceiver.java
@@ -80,39 +80,49 @@ public class ServerReceiver extends Thread {
 						break;
 					case "register":
 						String nickname = myClient.readLine(); // Matches FFFFF in ServerReceiver
-
-						if (!registeredUsers.contains(nickname)) {
-							registeredUsers.add(nickname);
-							System.out.println("User " + nickname + " registered.");
+						
+						if (nickname != null) {
+							if (!registeredUsers.contains(nickname)) {
+								registeredUsers.add(nickname);
+								System.out.println("User " + nickname + " registered.");
+							} else {
+								System.out.println(nickname + " is already registered.");
+							}
 						} else {
-							System.out.println(nickname + " is already registered.");
+							// No point in closing socket. Just give up.
+							return;
 						}
 						break;
 					case "login":
 						nickname = myClient.readLine(); // Matches FFFFF in ServerReceiver
+						if (nickname != null) {
+							if (loggedIn == false) {
+								if (registeredUsers.contains(nickname)) {
+									myClientsName = nickname;
 
-						if (loggedIn == false) {
-							if (registeredUsers.contains(nickname)) {
-								myClientsName = nickname;
+									// Assign the client's ID to an arraylist
+									ArrayList<Integer> extractedIDs = new ArrayList<Integer>();
 
-								// Assign the client's ID to an arraylist
-								ArrayList<Integer> extractedIDs = new ArrayList<Integer>();
+									// If statement to avoid nullpointexception
+									if (nicknameToIDMap.get(nickname) != null) {
+										extractedIDs = nicknameToIDMap.get(nickname);
+									}
+									extractedIDs.add(myClientsID);
+									nicknameToIDMap.put(nickname, extractedIDs);
 
-								// If statement to avoid nullpointexception
-								if (nicknameToIDMap.get(nickname) != null) {
-									extractedIDs = nicknameToIDMap.get(nickname);
+									loggedIn = true;
+									System.out.println("Client " + myClientsID + " successfully logged in as " + nickname);
+								} else {
+									System.out.println(nickname + " isn't registered. Please register first.");
 								}
-								extractedIDs.add(myClientsID);
-								nicknameToIDMap.put(nickname, extractedIDs);
-
-								loggedIn = true;
-								System.out.println("Client " + myClientsID + " successfully logged in as " + nickname);
 							} else {
-								System.out.println(nickname + " isn't registered. Please register first.");
+								System.out.println("Client " + myClientsID + " is already logged in to an account");
 							}
 						} else {
-							System.out.println("This client is already logged in to an account");
+							// No point in closing socket. Just give up.
+							return;
 						}
+
 						break;
 					case "logout":
 						if (loggedIn == true) {
@@ -127,10 +137,16 @@ public class ServerReceiver extends Thread {
 							System.out.println("Client " + myClientsID + " logged out of account " + myClientsName);
 							myClientsName = null;
 						} else {
-							System.out.println("Client not logged in, so can't be logged out");
+							System.out.println("Client " + myClientsID + " not logged in, so can't be logged out");
 						}
 					case "previous":
-
+						ArrayList<Message> extractedMessages;
+						extractedMessages = messageStore.get(myClientsName);
+						if (extractedMessages.size() != 0) {
+							
+						} else {
+							System.out.println("Client " + clientID " used command previoud however no messaged are stored for this client");
+						}
 						break;
 					case "next":
 						
@@ -141,41 +157,46 @@ public class ServerReceiver extends Thread {
 						break;
 					case "send":
 						String recipient = myClient.readLine(); // Matches DDDDD in ClientSender.java
-						String text = myClient.readLine(); // Matches EEEEE in ClientSender.java
+						if (recipient != null) {
+							String text = myClient.readLine(); // Matches EEEEE in ClientSender.java
 
-						if (text != null) {
-							if (nicknameToIDMap.get(recipient) == null) {
-								System.out.println("Message " + text + " to unexistent recipient " + recipient);
-							} else {
-								Message msg = new Message(myClientsName, text);
+							if (text != null &&) {
+								if (nicknameToIDMap.get(recipient) == null) {
+									System.out.println("Message " + text + " to unexistent recipient " + recipient);
+								} else {
+									Message msg = new Message(myClientsName, text);
 
-								// See how many client IDs there are with of the same name but different ID to
-								// allow a a user to have multiple copies running
-								ArrayList<Integer> extractedIDs = new ArrayList<Integer>();
-								extractedIDs = nicknameToIDMap.get(recipient);
+									// See how many client IDs there are with of the same name but different ID to
+									// allow a a user to have multiple copies running
+									ArrayList<Integer> extractedIDs = new ArrayList<Integer>();
+									extractedIDs = nicknameToIDMap.get(recipient);
 
-								for (Integer recipientID : extractedIDs) {
-									BlockingQueue<Message> recipientsQueue = clientTable.getQueue(recipientID); // Matches
-																												// EEEEE
-																												// in
-																												// ServerSender.java
+									for (Integer recipientID : extractedIDs) {
+										BlockingQueue<Message> recipientsQueue = clientTable.getQueue(recipientID); // Matches
+																													// EEEEE
+																													// in
+																													// ServerSender.java
 
-									if (recipientsQueue != null) {
-										recipientsQueue.offer(msg);
+										if (recipientsQueue != null) {
+											recipientsQueue.offer(msg);
+										}
 									}
+									
+									// Store message in server
+									ArrayList<Message> extractedMessages;
+									extractedMessages = messageStore.get(recipient);
+									if (extractedMessages.size() == 0) {
+										extractedMessages = new ArrayList<Message>();
+									}
+									extractedMessages.add(msg);
+									messageStore.put(recipient, extractedMessages);
+									
+									//Set the message that has just been sent to be the current message
+									currentMessageMap.put(recipient, extractedMessages.size() - 1);
 								}
-								
-								// Store message in server
-								ArrayList<Message> extractedMessages;
-								extractedMessages = messageStore.get(recipient);
-								if (extractedMessages.size() == 0) {
-									extractedMessages = new ArrayList<Message>();
-								}
-								extractedMessages.add(msg);
-								messageStore.put(recipient, extractedMessages);
-								
-								//Set the message that has just been sent to be the current message
-								currentMessageMap.put(recipient, extractedMessages.size() - 1);
+							} else {
+								// No point in closing socket. Just give up.
+								return;
 							}
 						} else {
 							// No point in closing socket. Just give up.
